c0b492de767b260f0cc803d0b631322dfc1e16e7
jason lim <50891910+Sxxov@users.noreply.github.com>
2023-10-20 22:27:43 +0800

    Add `lessons` & `brignull` related info

--- src/routes/building/lib/info/BuildingInfo.ts
+++ src/routes/building/lib/info/BuildingInfo.ts
@@ -4,6 +4,11 @@ import type { gltfs } from '../../../../assets/village/parts';
 export interface BuildingInfo {
 	name: string;
 	description: string;
+	lessons: readonly string[];
+	brignull: {
+		kind: string;
+		url: string;
+	};
 	icon: SvgString;
 	facade: (typeof gltfs)[keyof typeof gltfs];
 }
