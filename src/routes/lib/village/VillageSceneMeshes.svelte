8135b3d2a80eed20a7e46a7509d87ec43562c640
jason lim <50891910+Sxxov@users.noreply.github.com>
2023-12-04 19:33:14 +0800

    Don't include entire village gltfs into bundle

--- src/routes/lib/village/VillageSceneMeshes.svelte
+++ src/routes/lib/village/VillageSceneMeshes.svelte
@@ -8,15 +8,31 @@
 	import type { PartfulGltf } from '../../../lib/3d/part/PartfulGltf';
 	import HideOnOffscreen from '../../../lib/3d/optimisation/HideOnOffscreen.svelte';
 
-	const entries = <T extends Record<any, any>>(
-		object: T,
-		// eslint-disable-next-line @typescript-eslint/no-unsafe-return
-	): [keyof T, T[keyof T]][] => Object.entries(object) as any;
-
 	let keyAndPart: [keyof typeof gltfs, PartfulGltf][] = [];
 
-	entries(gltfs)
-		.filter(([k]) => k.startsWith('building'))
+	(
+		[
+			['building_1', gltfs.building_1],
+			['building_2', gltfs.building_2],
+			['building_3', gltfs.building_3],
+			['building_4', gltfs.building_4],
+			['building_5', gltfs.building_5],
+			['building_6', gltfs.building_6],
+			['building_7', gltfs.building_7],
+			['building_8', gltfs.building_8],
+			['building_9', gltfs.building_9],
+			['building_10', gltfs.building_10],
+			['building_11', gltfs.building_11],
+			['building_12', gltfs.building_12],
+			['building_13', gltfs.building_13],
+			['building_14', gltfs.building_14],
+			['building_15', gltfs.building_15],
+			['building_16', gltfs.building_16],
+		] as const satisfies [
+			keyof typeof gltfs,
+			(typeof gltfs)[keyof typeof gltfs],
+		][]
+	)
 		.map(async ([key, gltf]) => [key, await createPart(gltf)] as const)
 		.forEach(async (part) =>
 			part.then(([key, part]) => {
